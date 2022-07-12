3609290c8021d24d6c6045937cb4ebbce6c529ba
stefanatanasovmkd <atanasovstefan@hotmail.com>
2022-07-12 20:17:58 +0300

    commenting unused variable in Map.tsx

--- client/src/Map/Map.tsx
+++ client/src/Map/Map.tsx
@@ -7,15 +7,15 @@ import L from "leaflet"
 import Control from 'react-leaflet-custom-control'
 import BikesAvailable from './BikesAvailable';
 
-var greenIcon = L.icon({
-    iconUrl: 'https://img.icons8.com/office/344/street-view.png',
+// var greenIcon = L.icon({
+//     iconUrl: 'https://img.icons8.com/office/344/street-view.png',
 
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
+//     iconSize:     [38, 95], // size of the icon
+//     shadowSize:   [50, 64], // size of the shadow
+//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+//     shadowAnchor: [4, 62],  // the same for the shadow
+//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+// });
 
 
 export default function Map(): JSX.Element{
