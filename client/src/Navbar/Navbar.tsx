05d4d401538ccf0149fb6b6f3c52a31b9a58361a
stefanatanasovmkd <atanasovstefan@hotmail.com>
2022-07-20 19:15:36 +0300

    delete of unused imports in Navbar.tsx

--- client/src/Navbar/Navbar.tsx
+++ client/src/Navbar/Navbar.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@mui/material";
 import "./Style/Navbar.css";
 
 export default function Navbar(): JSX.Element {
   return (
     <div className="Navbar">
       <div className="Navbar-logo" onClick={() => window.location.reload()}>
-        {/* <span id="LogoText">Cycle Helsinki</span> */}
         <h1 id="LogoText">Cycle Helsinki</h1>
       </div>
     </div>
